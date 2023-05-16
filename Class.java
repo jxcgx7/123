12df3902669e933b3b8e82c44acf7d2e32d4174d
jxcgx7 <133723090+jxcgx7@users.noreply.github.com>
2023-05-16 09:27:00 +0800

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,15 @@
+package A;
+
+public class Class {
+	public static void main(String[]args){
+		String[] nameArray;
+		
+	String[] arr=new String[]{"张三","张四","张二","张一","张1"};
+	for(int i=0;i<arr.length;i++){
+		System.out.println(arr[i]);
+		int count=arr.length;
+		System.out.println(count);//元数个数
+	}
+
+	}
+	}
